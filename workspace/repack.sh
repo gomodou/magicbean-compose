ee2c0fc76a3d5929cc270f50ac5a7ab09b8ea78c
diggzhang <diggzhang@gmail.com>
2022-05-22 10:03:38 +0800

    add: repack script

---
+++ workspace/repack.sh
@@ -0,0 +1,6 @@
+version=$1
+tar -zxvf magicbean-$1.tar.gz
+rm -rf magicbean/spark-2.4.7-bin-hadoop2.7 magicbean/*.sh
+rm magicbean-$1.tar.gz
+tar -zcvf magicbean-$1.tar.gz ./magicbean/*
+cp ./magicbean/stem*.jar ./stem-$1.jar
