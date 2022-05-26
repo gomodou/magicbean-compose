0c7672c454c3116cdf8510c6bd7702a05867ce4b
diggzhang <diggzhang@gmail.com>
2022-05-26 11:44:00 +0800

    update：oss bucket config

--- workspace/repack.sh
+++ workspace/repack.sh
@@ -4,3 +4,4 @@ rm -rf magicbean/spark-2.4.7-bin-hadoop2.7 magicbean/*.sh
 rm magicbean-$1.tar.gz
 tar -zcvf magicbean-$1.tar.gz ./magicbean/*
 cp ./magicbean/stem*.jar ./stem-$1.jar
+./ossutil64 cp magicbean-2.6.4.tar.gz oss://hykpi-release/hykpi-release
