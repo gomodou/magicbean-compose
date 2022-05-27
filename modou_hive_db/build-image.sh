56beb602360ad615021ffe11d82f8e3087c2321b
diggzhang <diggzhang@gmail.com>
2022-05-27 09:05:29 +0800

    update: rename modou to hykpi

--- modou_hive_db/build-image.sh
+++ modou_hive_db/build-image.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 set -o pipefail
-IMAGE=hykpi/modou_hive_db
+IMAGE=hykpi/hykpi_hive_db
 VERSION=$1
 docker build -t ${IMAGE}:${VERSION} . 
 ID=$(docker image ls | grep $IMAGE | grep $VERSION | awk '{print $3}')
 docker tag $ID ${IMAGE}:latest
-docker tag $ID registry.cn-shanghai.aliyuncs.com/magicbean/modou_hive_db:latest
-docker tag $ID registry.cn-shanghai.aliyuncs.com/magicbean/modou_hive_db:$VERSION
+docker tag $ID registry.cn-shanghai.aliyuncs.com/magicbean/hykpi_hive_db:latest
+docker tag $ID registry.cn-shanghai.aliyuncs.com/magicbean/hykpi_hive_db:$VERSION
 docker images | grep ${IMAGE}
